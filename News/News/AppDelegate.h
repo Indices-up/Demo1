b6be4bba39fd7543420125bedc78f70fac9f71f6
jinxs <305146060@qq.com>
2016-04-23 18:43:59 +0800

    初始化项目

---
+++ News/News/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  News
+//
+//  Created by jinxs on 16/4/23.
+//  Copyright © 2016年 MrJin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ News/News/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  News
+//
+//  Created by jinxs on 16/4/23.
+//  Copyright © 2016年 MrJin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
