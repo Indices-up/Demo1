545e6cbf0b60af20ce6d997e8c71231bd1ed43c6
jinxs <305146060@qq.com>
2016-04-23 22:32:14 +0800

    搭建项目框架

--- News/News/Classes/News/NewsTableViewController.h
+++ News/News/Classes/News/NewsTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  NewsTableViewController.h
 //  News
 //
 //  Created by jinxs on 16/4/23.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface NewsTableViewController : UITableViewController
 
 @end
-
